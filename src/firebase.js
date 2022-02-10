e91dfa5284aa641cdc2c7abc71c0796a1f93bec5
jaffasunny <jaffer.sunny125@gmail.com>
2022-02-10 16:18:30 +0500

    Completed

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,13 @@
 import firebase from "firebase";
-require("dotenv").config({ path: "../.env" });
 
 const firebaseApp = firebase.initializeApp({
-	apiKey: process.env.API_KEY,
-	authDomain: process.env.AUTH_DOMAIN,
-	projectId: process.env.PROJECT_ID,
-	storageBucket: process.env.STORAGE_BUCKET,
-	messagingSenderId: process.env.MESSAGING_SENDER_ID,
-	appId: process.env.APP_ID,
-	measurementId: process.env.MEASUREMENT_ID,
+	apiKey: "AIzaSyCS-ztnVGN4VnSXpc2hdY0M2IuhtWax530",
+	authDomain: "instagram-clone-reactapp-db0d8.firebaseapp.com",
+	projectId: "instagram-clone-reactapp-db0d8",
+	storageBucket: "instagram-clone-reactapp-db0d8.appspot.com",
+	messagingSenderId: "658590851498",
+	appId: "1:658590851498:web:fdadc018169e3051ef0bd3",
+	measurementId: "G-N7PYTWKW1X",
 });
 
 const db = firebaseApp.firestore();
